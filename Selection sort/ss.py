f66aa3a6ff8e3880909ed0fd3e402aa1148c27c0
Sri Balaji <55683994+Balaji751@users.noreply.github.com>
2020-06-09 19:49:21 +0530

    Selection sort in both python and java

---
+++ Selection sort/ss.py
@@ -0,0 +1,11 @@
+a=list(map(int,input().split()))
+for i in range(len(a)):
+	min_index=i
+	for j in range(i+1,len(a)):
+		if a[min_index]>a[j]:
+			min_index=j
+	a[i],a[min_index]=a[min_index],a[i]
+
+print(*a)
+
+
